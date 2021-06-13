35fb9af08a1025cb85c5e654fad7297c2ac56346
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-06-14 06:52:56 +0800

    Update submit.sh

--- Optimize/submit/submit.sh
+++ Optimize/submit/submit.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=80
-#SBATCH --time=00300:00
+#SBATCH --time=00:20:00
 #SBATCH --job-name=mpi_job
-#SBATCH --output=gpaw_one_node_4.txt
+#SBATCH --output=gpaw_four_node_1.txt
 #SBATCH --mail-user=n88199911652@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-
-export APPROOT=$APPROOT
-export MODULEPATH=$APPROOT/modules:$MODULEPATH
 module load intel/2019u4
 module load intelmpi/2019u4
 module load openblas/0.3.7
 module load python/3.8.5
-module load libxc-4.3.4
+module load fftw-yi
 
-cd  $APPROOT
-
+source /scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/path.sh
+cd  /scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/scalapack_4
 export PATH=$PATH:$HOME/.local/bin
-export GPAW_CONFIG=$APPROOT/gpaw-20.10.0/siteconfig.py
-export GPAW_SETUP_PATH=$APPROOT/gpaw-setups-0.9.20000
 
+export PATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/bin:$PATH
+export LD_LIBRARY_PATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/lib:$LD_LIBRARY_PATH
+export CPATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/include:$CPATH
+
+echo "============================ ipm using ========================="
+#export I_MPI_STATS=ipm
+#export I_MPI_STATS_FILE=profile.txt
+export I_MPI_DEVICE=rdssm
+export OMP_NUM_THREADS=2
+#export MKL_NUM_THREADS=2
 
 HOSTS=.hosts-job$SLURM_JOB_ID
 HOSTFILE=.hostlist-job$SLURM_JOB_ID
@@ -31,4 +36,5 @@ sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 
 echo "=============================mpi run============================"
-mpirun -np 320 -hostfile ./$HOSTFILE gpaw-python $APPROOT/gpaw-isc-2021/input-files/copper.py
+#mpirun -np 80 gpaw python /home/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/gpaw-isc-2021/input-files/copper.py
+mpiexec -np 160 --bind-to numa -hostfile ./$HOSTFILE  gpaw python /home/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/gpaw-isc-2021/input-files/copper.py
