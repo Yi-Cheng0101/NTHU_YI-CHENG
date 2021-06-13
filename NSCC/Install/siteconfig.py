c75a9fc4ee942c02ec873da917139460213c9710
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-06-14 04:54:40 +0800

    Update siteconfig.py

--- NSCC/Install/siteconfig.py
+++ NSCC/Install/siteconfig.py
@@ -2,7 +2,7 @@ compiler = '$APPROOT/opt/mpich-3.3/bin/mpicc'
 mpicompiler = '$APPROOT/opt/mpich-3.3/bin/mpicc'
 mpilinker = '$APPROOT/opt/mpich-3.3/bin/mpicc'
 
-xc = '/$APPROOT/opt/libxc-4.3.4/'
+xc = '$APPROOT/opt/libxc-4.3.4/'
 include_dirs += [xc + 'include']
 extra_link_args += [xc + 'lib/libxc.a']
 if 'xc' in libraries:
@@ -11,4 +11,4 @@ if 'xc' in libraries:
 
 
 parallel_python_interpreter = False
-extra_link_args += ['$AAPPROOT/lib/BLAS-3.8.0/libblas.a']
+extra_link_args += ['$APPROOT/lib/BLAS-3.8.0/libblas.a']
