030dff64c8b2e2164a678fa93bb08bb07d3de8d7
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-06-14 06:59:00 +0800

    Update siteconfig.py

--- Optimize/Install/siteconfig.py
+++ Optimize/Install/siteconfig.py
@@ -2,7 +2,6 @@ compiler = 'icc'
 mpicompiler = 'mpicc'
 mpilinker = 'mpicc'
 
-# - static linking:
 xc = '/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/libxc-4.3.4/'
 include_dirs += [xc + 'include']
 extra_link_args += [xc + 'lib/libxc.a']
@@ -42,16 +41,11 @@ if elpa:
 extra_link_args += ['-lfftw3_mpi']
 extra_link_args += ['-lfftw3']
 extra_link_args += ['-lifcore']
-#include_dirs += ['/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/scala/elpa-2019.11.001/include/elpa-2019.11.001/elpa']
-
 extra_link_args += ['/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/scala/libvdwxc-0.4.0/lib/libvdwxc.a']
 
 
 fftw = True
-#libraries += ['fftw3']
-#extra_compile_args += ['-xCORE-AVX512']
 extra_link_args += ['/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/scala/elpa-2019.11.001/lib/libelpa.a']
-#include_dirs += ['/scinet/niagara/software/2019b/opt/intel-2019u4-intelmpi-2019u4/fftw/3.3.8/include']
 include_dirs += ['/scinet/intel/2019u4/compilers_and_libraries_2019.4.243/linux/mkl/include/fftw']
 extra_compile_args += ['/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/scala/fftw3xf/libfftw3xf_intel.a']
 extra_compile_args += ['-O3','-qopenmp']
@@ -61,8 +55,5 @@ extra_link_args += ['-qopenmp']
 
 libraries += ['mkl_rt']
 libraries = ['mkl_intel_thread','mkl_intel_lp64', 'mkl_sequential', 'mkl_core','mkl_lapack95_lp64', 'mkl_scalapack_lp64','mkl_blacs_intelmpi_ilp64']
-#extra_link_args += ['-L/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/didwdidw/IPM/lib -lipm']
-#extra_link_args += ['-L/scinet/niagara/software/2019b/opt/intel-2019u4-intelmpi-2019u4/scalapack/2.0.2/lib']
 parallel_python_interpreter = False
 
-#extra_link_args += ['/scinet/niagara/software/2019b/opt/intel-2019u4/openblas/0.3.7/lib/libopenblas.a']
