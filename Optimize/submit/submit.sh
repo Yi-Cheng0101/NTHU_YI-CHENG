becfbe377c03f9ae7c4466667e66c098359d5ef7
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-06-17 10:46:38 +0800

    Update submit.sh

--- Optimize/submit/submit.sh
+++ Optimize/submit/submit.sh
@@ -6,27 +6,24 @@
 #SBATCH --output=gpaw_four_node_1.txt
 #SBATCH --mail-user=n88199911652@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
+export APPROOT=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/gpaw
+export MODULEPATH=$APPROOT/modules:$MODULEPATH
+export GPAW_CONFIG=$APPROOT/gpaw-20.10.0/siteconfig.py
+export GPAW_SETUP_PATH=$APPROOT/gpaw-setups-0.9.20000
 
 module load intel/2019u4
 module load intelmpi/2019u4
-module load openblas/0.3.7
 module load python/3.8.5
-module load fftw-yi
+module load fftw-mkl
+module load libxc-4.3.4
+module load hdf5-1.8.21
+module load libvdxvc-0.4.0
+module load elpa-2021.05.001
 
-source /scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/path.sh
-cd  /scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/scalapack_4
+cd  $APPROOT
 export PATH=$PATH:$HOME/.local/bin
-
-export PATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/bin:$PATH
-export LD_LIBRARY_PATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/lib:$LD_LIBRARY_PATH
-export CPATH=/scratch/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/opt/hdf5-1.8.21/include:$CPATH
-
-echo "============================ ipm using ========================="
-#export I_MPI_STATS=ipm
-#export I_MPI_STATS_FILE=profile.txt
-export I_MPI_DEVICE=rdssm
 export OMP_NUM_THREADS=2
-#export MKL_NUM_THREADS=2
+
 
 HOSTS=.hosts-job$SLURM_JOB_ID
 HOSTFILE=.hostlist-job$SLURM_JOB_ID
@@ -36,5 +33,4 @@ sort $HOSTS | uniq -c | awk '{print $2 ":" $1}' >> $HOSTFILE
 
 
 echo "=============================mpi run============================"
-#mpirun -np 80 gpaw python /home/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/gpaw-isc-2021/input-files/copper.py
-mpiexec -np 160 --bind-to numa -hostfile ./$HOSTFILE  gpaw python /home/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/gpaw-isc-2021/input-files/copper.py
+mpiexec -np 160 -bind-to none -hostfile ./$HOSTFILE  gpaw python /home/l/lcl_uotiscscc/lcl_uotiscsccs1040/yicheng/gpaw-isc-2021/input-files/copper.py
